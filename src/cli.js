ae14ee3937d7c87d0a488634e307dc0e7db86dc0
Владислав <traize@MacBook-Air-Vladislav.local>
2024-02-29 01:19:05 +0300

    upgrade

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const getUserName = () => {
+
+   const name = readlineSync.question('May I have your name? ');
+
+
+return name
+}
+
+export default getUserName
