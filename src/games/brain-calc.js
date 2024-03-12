64ac2a375dce2994ffb966cff72846327972d22a
Владислав <traize@MacBook-Air-Vladislav.local>
2024-03-12 15:17:43 +0300

    add failes

---
+++ src/games/brain-calc.js
@@ -0,0 +1,39 @@
+import gameStart from '../indexCalc.js'
+import randomNumber from '../helper.js'
+
+const description = 'What is the result of the expression?'
+const getQuestionAndAnswer = () => { 
+let num1 = randomNumber()
+let num2 = randomNumber()
+
+const randomSign = Math.floor(Math.random() * 3)
+let answer
+const signs = ['+', '-', '*'];
+const sign = signs[randomSign]
+
+switch (sign) {
+    case '+':
+       answer = num1 + num2; 
+        break
+    
+    case '-':
+       answer = num1 - num2;   
+        break
+    
+    case '*':
+       answer = num1 * num2;   
+        break   
+        
+    default:
+       answer = null 
+} 
+      
+	const question = `${num1} ${sign} ${num2}`;
+	const correctAnswer = answer
+  
+  return [question, correctAnswer]
+}
+
+export default () => {
+  gameStart(description, getQuestionAndAnswer)
+}
---
+++ src/games/brain-even.js
@@ -0,0 +1,22 @@
+import gameStart from '../index.js'
+import randomNumber from '../helper.js'
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+const getEven = (num) => {
+	if (num % 2 === 0) {
+		return true
+	} else {
+		return false
+	}
+}
+
+const getQuestionAndAnswer = () => {     
+	const question = randomNumber();
+	const correctAnswer = getEven(question) === true ? 'yes' : 'no'
+	return [question, correctAnswer]
+}
+
+export default () => {
+    gameStart(description, getQuestionAndAnswer);
+}
