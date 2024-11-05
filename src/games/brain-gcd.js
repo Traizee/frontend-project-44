611a08ef92e608bae76e993145f20342491ca62a
Владислав <traize@MacBook-Air-Vladislav.local>
2024-11-05 17:48:31 +0300

    upgrade gcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -3,10 +3,6 @@ import randomNumber from '../helper.js'
 
 const description = 'Find the greatest common divisor of given numbers.'
 
-const getQuestionAndAnswer = () => {  
-    let num1 = randomNumber()
-    let num2 = randomNumber()
-
 const gcd = (a, b) => {
     if(a === 0) {
         return b
@@ -14,6 +10,11 @@ const gcd = (a, b) => {
         return gcd (b % a, a)
     }
 }
+
+const getQuestionAndAnswer = () => {  
+    let num1 = randomNumber()
+    let num2 = randomNumber()
+
 	const question = `${num1} ${num2}`
 	const correctAnswer = gcd(num1, num2)
 	return [question, correctAnswer]
