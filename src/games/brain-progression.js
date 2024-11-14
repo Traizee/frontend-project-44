a0805f6054f621f0a8759d8f2039d2cbe8103645
Владислав <traize@MacBook-Air-Vladislav.local>
2024-11-14 21:20:56 +0300

    upgrade progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,26 +3,32 @@ import randomNumber from '../helper.js'
 
 const description = 'What number is missing in the progression?'
 
+
+const createArray = () => {
+    const firstNum = randomNumber()
+    let array = [firstNum, 0, 0, 0, 0, 0, 0,]
+    const randomNumProgression = randomNumber() 
+    
+    for (let i = 1; i < array.length; i += 1) {
+        const arrayItem = array[i - 1];
+
+        const arrayItemProgression = arrayItem + randomNumProgression;
+        array[i] = arrayItemProgression
+}
+    return array
+}
+
+
 const getQuestionAndAnswer = () => {
 
     const hiddenValue = Math.floor(Math.random() * 6)
-    const randomNumProgression = randomNumber()
 
-    const firstNum = randomNumber()
-    let array = [firstNum, 0, 0, 0, 0, 0, 0,]
+    let arr = createArray();
+    let arrNew = arr.slice();
+    arrNew[hiddenValue] = '\'..\''
 
-        for (let i = 1; i < array.length; i += 1) {
-            const arrayItem = array[i - 1];
-
-            const arrayItemProgression = arrayItem + randomNumProgression;
-            array[i] = arrayItemProgression
-    }
-
-    let arr = array.slice();
-    arr[hiddenValue] = '\'..\''
-
-    const question = arr.join(' ')
-	const correctAnswer = array[hiddenValue]
+    const question = arrNew.join(' ')
+	const correctAnswer = arr[hiddenValue]
 	return [question, correctAnswer]
 
 }
