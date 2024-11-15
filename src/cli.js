a5d57342b4989e8f6f9af7b8a5308984fcfcdaf8
Владислав <traize@MacBook-Air-Vladislav.local>
2024-11-15 16:54:06 +0300

    upgrade games

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,13 @@
 import readlineSync from 'readline-sync';
 
+const welcome = () => {
+   console.log('Welcome to the Brain Games!')
+   
+   const userName = getUserName();
+   console.log(`Hello, ${userName}!`);
+   return userName
+   }
+
 const getUserName = () => {
 
    const name = readlineSync.question('May I have your name? ');
