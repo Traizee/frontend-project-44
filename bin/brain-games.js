f14b21db4e906c0cc6189acc194bfa81c0f3eddc
Владислав <traize@MacBook-Air-Vladislav.local>
2024-11-15 17:03:57 +0300

    up games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,13 +1,7 @@
 #!/usr/bin/env node
 import getUserName from "../src/cli.js"
 
-export const welcome = () => {
-console.log('Welcome to the Brain Games!')
-
-const userName = getUserName();
-console.log(`Hello, ${userName}!`);
-return userName
-}
+getUserName()
 
 
 
