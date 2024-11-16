aceb4cc85301f95de785267a0ba5ce5dbb4e6fed
Владислав <traize@MacBook-Air-Vladislav.local>
2024-11-16 22:22:21 +0300

    up cli

--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const welcome = () => {
-   console.log('Welcome to the Brain Games!')
-   
-   const userName = getUserName();
-   console.log(`Hello, ${userName}!`);
-   return userName
-   }
-
-const getUserName = () => {
-
-   const name = readlineSync.question('May I have your name? ');
-
-
-return name
-}
-
-export default getUserName
+export default () => {
+  const username = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${username}!`);
+};
