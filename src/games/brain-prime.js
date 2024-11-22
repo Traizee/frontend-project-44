b41f63e474a87af3ec01ea0ae14ab6908104b5c8
Владислав <traize@MacBook-Air-Vladislav.local>
2024-11-22 15:55:25 +0300

    up lint

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,13 +4,13 @@ import randomNumber from '../helper.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const getPrime = (num) => {
-  if (num <= 1) { return false };
-  if (num === 2) { return true };
-  if (num % 2 === 0) { return false };
+  if (num <= 1) { return false; }
+  if (num === 2) { return true; }
+  if (num % 2 === 0) { return false; }
 
   const sqrt = Math.sqrt(num);
   for (let i = 3; i <= sqrt; i += 2) {
-    if (num % i === 0) { return false };
+    if (num % i === 0) { return false; }
   }
   return true;
 };
--- src/helper.js
+++ src/helper.js
@@ -1,1 +1,3 @@
-export default () => {return Math.floor(Math.random() * 13)};
+export default () => {
+  return Math.floor(Math.random() * 13);
+};
